3c83c3522db541a7367c7b2690a0faf10ced8368
Jui <siwa.boonpun@gmail.com>
2019-11-15 10:18:14 +0700

    first commit

---
+++ anki_udp_client.py
@@ -0,0 +1,32 @@
+from overdrive import Overdrive
+import socket
+
+tmp_clockwise = True
+tmp_piece = 0
+
+def locationChangeCallback(addr, location, piece, speed, clockwise):
+    global tmp_clockwise, tmp_piece
+    print(" Location from " + addr + " : " + "Piece=" +
+          str(piece) + " Location=" + str(location) + " Clockwise=" + str(clockwise))
+    tmp_clockwise = str(clockwise)
+    tmp_piece = str(piece)
+
+
+# UDP
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+client.settimeout(0.1)
+client.bind(("", 37020))
+
+# Anki
+car = Overdrive("E2:81:E9:52:65:97")
+car.setLocationChangeCallback(locationChangeCallback)
+car.changeSpeed(200, 300)
+
+while True:
+    message = str(tmp_clockwise) + " " + str(tmp_piece)
+    client.sendto(message, ('172.27.0.3', 44444))
+    data, addr = client.recvfrom(1024)
+    if data == "0":
+        car.changeSpeed(0, 1000)
+input()
---
+++ anki_udp_server.py
@@ -0,0 +1,33 @@
+import socket
+import time
+
+addr0 = "172.27.0.4"
+addr1 = "172.27.0.5"
+
+anki0 = {"clockwise": True, "piece": 0}
+anki1 = {"clockwise": True, "piece": 0}
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+server.bind(("", 44444))
+
+while True:
+    data, addr = server.recvfrom(1024)
+    clock_wise, piece = str(data).strip().split()
+    clock_wise = clock_wise[:-1]
+    piece = piece[:-1]
+
+    # Save data part
+    if addr == addr0:
+        anki0["clockwise"] = clock_wise
+        anki0["piece"] = piece
+
+    if addr == addr1:
+        anki1["clockwise"] = clock_wise
+        anki1["piece"] = piece
+
+    # Logic part
+    if ((anki0["piece"] == 17 and anki1["piece"] == 40) or (anki0["piece"] == 40 and anki1["piece"] == 17)) and anki0["clockwise"] != anki1["clockwise"]:
+        message = b"0"
+        server.sendto(message, (addr0, 37020))
+        server.sendto(message, (addr1, 37020))
